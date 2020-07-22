d478b50d42cb57076936c569e5ebe659bb1b756d
Luke Todd <luketodd@Luke-Todd--luketoddhappyhippoherbalscom--MacBookPro.local>
2020-07-22 09:51:25 -0600

    boom baby

--- numbers.js
+++ numbers.js
@@ -5,14 +5,14 @@ let numbers = []
 function sixCombo() {
 
     let template = '<ul>'
-    for(let i = 200000; i <= 200100; i++){
+    for(let i = 200000; i <= 299999; i++){
         numbers.push(i)
+        }   
         numbers.forEach( number => {
             template += '<li>' + number + '</li>'
         })
         template += '</ul>'
         document.getElementById('displayNum').innerHTML = template
-        }   
     }
 
 
