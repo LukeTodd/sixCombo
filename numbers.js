a2ab88ed054adaa3c3e80ea9493002af5e7cebab
Luke Todd <luketodd@Luke-Todd--luketoddhappyhippoherbalscom--MacBookPro.local>
2020-07-22 09:42:03 -0600

    changed range

--- numbers.js
+++ numbers.js
@@ -5,7 +5,7 @@ let numbers = []
 function sixCombo() {
 
     let template = '<ul>'
-    for(let i = 200000; i <= 200050; i++){
+    for(let i = 200000; i <= 200100; i++){
         numbers.push(i)
         numbers.forEach( number => {
             template += '<li>' + number + '</li>'
