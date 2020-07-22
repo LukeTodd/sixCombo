ca8e1f8a90b9f7578d151f40120da0fdca949473
Luke Todd <luketodd@Luke-Todd--luketoddhappyhippoherbalscom--MacBookPro.local>
2020-07-22 08:58:31 -0600

    new

---
+++ numbers.js
@@ -0,0 +1,12 @@
+
+
+function sixCombo() {
+    let num = 0    
+    for(let i = 200000; i <= 299999; i++){
+        num = i
+        document.getElementById('displayNum').innerText = num
+        }   
+    }
+
+
+
